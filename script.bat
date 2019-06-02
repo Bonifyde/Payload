ddb17ad0af2a334a1e533652e02176d025db3941
Bonifyde <45215148+Bonifyde@users.noreply.github.com>
2019-06-02 19:52:28 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,1 @@
-powershell -windowstyle hidden (New-Object -com "WPlayer.OCX7").cdromcollection.item(0).eject()
+powershell -windowstyle hidden (New-Object -com "WMPlayer.OCX.7").cdromcollection.item(0).eject()
